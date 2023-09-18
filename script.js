3f25697fcba9d1e7f6fe7e5a9cd054e2014b04dc
senger-lab <142671959+senger-lab@users.noreply.github.com>
2023-09-17 21:19:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 function showTab(tabId) {
-    // Prevent default link behavior
     event.preventDefault();
 
-    // Remove active classes from previous tab contents and tabs
-    document.querySelector('.tab-content.active').classList.remove('active');
-    const activeTab = document.querySelector('nav a.active');
-    if (activeTab) {
-        activeTab.classList.remove('active');
-    }
-
-    // Add active class to the selected tab content
-    document.getElementById(tabId).classList.add('active');
-
-    // Add active class to the clicked tab
-    const tabs = document.querySelectorAll('nav a');
-    tabs.forEach(tab => {
-        if (tab.hasAttribute('onclick') && tab.getAttribute('onclick').includes(tabId)) {
-            tab.classList.add('active');
-        }
+    // Hide all tab contents
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.style.display = 'none';
     });
+    
+    // Show the selected tab content
+    document.getElementById(tabId).style.display = 'block';
 }
